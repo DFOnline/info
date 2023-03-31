4a9cfe3b2599ec736c63ac3a26ca558afc45d8fe
GeorgeRNG <grogrng@gmail.com>
2023-03-31 20:20:10 +0100

    Add content folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
